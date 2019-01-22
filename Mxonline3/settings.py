46ac0d46a0bb76faa9344807f2e5252bc4e96938
Neil Wang <985341237@qq.com>
2019-01-22 18:00:23 +0800

    编写前端页面base.html未完成

--- Mxonline3/settings.py
+++ Mxonline3/settings.py
@@ -144,6 +144,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES = [
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
